da1b912e215c0499130e52513044070ad15c8a65
seba <seba.chavez21@gmail.com>
2020-02-28 17:09:53 -0300

    animaciones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
@@ -8,9 +8,50 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 })
 export class HomeComponent implements OnInit {
   faCoffee= faCoffee
-  constructor() { }
+  initApp:Boolean = false
+  showSectionPostres:Boolean = false
+  constructor() { 
+    setTimeout(() => {
+      this.initApp = true
+    },1000)
+  }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // if (document.body.scrollTop > 20 ||     
+    //   document.documentElement.scrollTop > 20) {
+    //     document.getElementById('linkServices').classList.add('active')
+    //     document.getElementById('mainNav').classList.add('navbar-shrink');
+    //     document.getElementById('linkAbout').classList.remove('active')
+    // } else {
+    //   document.getElementById('mainNav').classList.remove('navbar-shrink');
+    //   document.getElementById('linkServices').classList.remove('active')
+    //   document.getElementById('linkAbout').classList.remove('active')
+    // }
+    if(document.body.scrollTop > document.getElementById('sectionPostres').offsetTop ||     
+        document.documentElement.scrollTop > document.getElementById('about').offsetTop){
+    }
+    if(document.body.scrollTop > document.getElementById('portafolio').offsetTop ||     
+    document.documentElement.scrollTop > document.getElementById('portafolio').offsetTop){
+      document.getElementById('linkPortafolio').classList.add('active')
+      document.getElementById('linkAbout').classList.remove('active')
+    } else {
+      document.getElementById('linkPortafolio').classList.remove('active')
+    }
+  }
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    // if (scrollPosition >= this.topPosToStartShowing) {
+    //   this.isShow = true;
+    // } else {
+    //   this.isShow = false;
+    // }   
   }
 
 }
