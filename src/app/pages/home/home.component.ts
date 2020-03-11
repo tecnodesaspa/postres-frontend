c739c4381a4c9539b2135fb896db6305bd91911d
Sebastian Chavez <seba.chavez21@gmail.com>
2020-03-10 21:07:33 -0300

    fetch

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import data from './postres.json'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   faCoffee= faCoffee
   initApp:Boolean = false
   showSectionPostres:Boolean = false
+  showSectionImages:Boolean = false
+  showTitleImages:Boolean = false
+  showSectionPromo:Boolean = false
+  showMenu:Boolean = false
   constructor() { 
     setTimeout(() => {
       this.initApp = true
@@ -21,37 +26,45 @@ export class HomeComponent implements OnInit {
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    // if (document.body.scrollTop > 20 ||     
-    //   document.documentElement.scrollTop > 20) {
-    //     document.getElementById('linkServices').classList.add('active')
-    //     document.getElementById('mainNav').classList.add('navbar-shrink');
-    //     document.getElementById('linkAbout').classList.remove('active')
-    // } else {
-    //   document.getElementById('mainNav').classList.remove('navbar-shrink');
-    //   document.getElementById('linkServices').classList.remove('active')
-    //   document.getElementById('linkAbout').classList.remove('active')
-    // }
-    if(document.body.scrollTop > document.getElementById('sectionPostres').offsetTop ||     
-        document.documentElement.scrollTop > document.getElementById('about').offsetTop){
-    }
-    if(document.body.scrollTop > document.getElementById('portafolio').offsetTop ||     
-    document.documentElement.scrollTop > document.getElementById('portafolio').offsetTop){
-      document.getElementById('linkPortafolio').classList.add('active')
-      document.getElementById('linkAbout').classList.remove('active')
+    if(document.body.scrollTop + 200 > document.getElementById('sectionPostres').offsetTop ||     
+        document.documentElement.scrollTop + 200 > document.getElementById('sectionPostres').offsetTop){
+          this.showSectionPostres = true
     } else {
-      document.getElementById('linkPortafolio').classList.remove('active')
+      this.showSectionPostres = false
     }
+    if(document.body.scrollTop + 200 > document.getElementById('sectionTitleImages').offsetTop ||     
+        document.documentElement.scrollTop + 200 > document.getElementById('sectionTitleImages').offsetTop){
+          this.showTitleImages = true
+    } else {
+      this.showTitleImages = false
+    }
+    if(document.body.scrollTop + 200 > document.getElementById('sectionImages').offsetTop ||     
+        document.documentElement.scrollTop + 200 > document.getElementById('sectionImages').offsetTop){
+          this.showSectionImages = true
+    } else {
+      this.showSectionImages = false
+    }
+    if(document.body.scrollTop + 200 > document.getElementById('sectionPromo').offsetTop ||     
+        document.documentElement.scrollTop + 200 > document.getElementById('sectionPromo').offsetTop){
+          this.showSectionPromo = true
+    } else {
+      this.showSectionPromo = false
+    }
+    // if(document.body.scrollTop + 200 > document.getElementById('sectionPromo').offsetTop ||     
+    //     document.documentElement.scrollTop + 200 > document.getElementById('sectionPromo').offsetTop){
+    //       this.showSectionPromo = true
+    // } else {
+    //   this.showSectionPromo = false
+    // }
   }
   checkScroll() {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
     console.log('[scroll]', scrollPosition);
-    
-    // if (scrollPosition >= this.topPosToStartShowing) {
-    //   this.isShow = true;
-    // } else {
-    //   this.isShow = false;
-    // }   
+  }
+
+  openMenu(){
+    this.showMenu = !this.showMenu
   }
 
 }
